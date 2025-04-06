e9fe7e5441d8a1e59249fd87ff65d9b8cacc76a2
Nikolai <micurino@gmail.com>
2025-04-06 13:25:21 -0700

    fix UI build path for ExpressJS routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,17 @@
 import path from 'node:path';
 import { Express } from 'express';
 
+const UIBuildPath = path.resolve(__dirname, '../../dist');
+
 export default function routes(app: Express) {
     app.use('/assets', (req, res) => {
-        res.sendFile(path.resolve(__dirname, `../dist/assets/${req.url}`));
+        res.sendFile(path.resolve(UIBuildPath, `assets/${req.url}`));
     });
     app.use(['/favicon.svg', '/favicon.png', '/favicon.ico'], (req, res) => {
-        res.sendFile(path.resolve(__dirname, `../dist/${req.url}`));
+        res.sendFile(path.resolve(UIBuildPath, req.url));
     });
 
     app.use('/', (_, res) => {
-        res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+        res.sendFile(path.resolve(UIBuildPath, 'index.html'));
     });
 }
