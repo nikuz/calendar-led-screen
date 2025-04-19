cbc0937e839cc3d58b03e5fbba812726bd466f4b
Nikolai <micurino@gmail.com>
2025-04-19 12:43:00 -0700

    server: improve assets routing with express.js v5

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import fs from 'node:fs';
 import type { Express } from 'express';
 import { calendarControllers } from './controllers/index.ts';
 import { __DIRNAME } from './constants.ts';
@@ -6,13 +7,15 @@ import { __DIRNAME } from './constants.ts';
 const UIBuildPath = path.resolve(__DIRNAME, '../../dist');
 
 export default function routes(app: Express) {
-    app.use('/assets', (req, res) => {
-        res.sendFile(path.resolve(UIBuildPath, `assets/${req.url}`));
+    app.use(['/assets', '/favicon.*any', '/{*name}.ttf', '/{*name}.woff2'], (req, res) => {
+        const assetPath = path.join(UIBuildPath, req.originalUrl);
+        if (!fs.existsSync(assetPath)) {
+            res.status(404).send();
+        } else {
+            res.sendFile(path.join(UIBuildPath, req.originalUrl));
+        }
     });
-    app.use(['/favicon.svg', '/favicon.png', '/favicon.ico'], (req, res) => {
-        res.sendFile(path.resolve(UIBuildPath, req.url));
-    });
-    
+
     app.use(['/', '/index.html'], (_, res) => {
         res.sendFile(path.resolve(UIBuildPath, 'index.html'));
     });
