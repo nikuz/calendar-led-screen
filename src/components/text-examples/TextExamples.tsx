d5d1bf403c07393f9f736ce633d3a9a6b73a36be
Nikolai <micurino@gmail.com>
2025-04-20 12:22:11 -0700

    ui: add PPNeueBit and PPMondwest fonts

--- src/components/text-examples/TextExamples.tsx
+++ src/components/text-examples/TextExamples.tsx
@@ -8,6 +8,18 @@ export function TextExamples() {
                 <div class="tsct-unscii-16">20:56</div>
                 <div class="tsct-unscii-16-large">20:56</div>
             </div>
+
+            <div>
+                <div class="tsct-PPMondwest-8">20:56</div>
+                <div class="tsct-PPMondwest-16">20:56</div>
+                <div class="tsct-PPMondwest-16-large">20:56</div>
+            </div>
+
+            <div>
+                <div class="tsct-PPNeueBit-8">20:56</div>
+                <div class="tsct-PPNeueBit-16">20:56</div>
+                <div class="tsct-PPNeueBit-16-large">20:56</div>
+            </div>
         </div>
     );
 }
