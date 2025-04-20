c367586ef8ca83ec83dd1c4c4cd34b5a6c7d4ed3
Nikolai <micurino@gmail.com>
2025-04-20 12:29:03 -0700

    ui: add font size adjust logic on text examples screen

--- src/components/text-examples/TextExamples.tsx
+++ src/components/text-examples/TextExamples.tsx
@@ -1,6 +1,30 @@
+import { createSignal, onMount, onCleanup } from 'solid-js';
 import './TextExamples.css';
 
 export function TextExamples() {
+    const [dynamicFontSize, setTerminusFontSize] = createSignal(8);
+
+    const keydownHandler = (event: KeyboardEvent) => {
+        const step = event.shiftKey ? 1 : 0.1;
+        let newFontSize = dynamicFontSize();
+
+        if (event.code === 'ArrowUp') {
+            newFontSize += step;
+        } else if (event.code === 'ArrowDown') {
+            newFontSize -= step;
+        }
+
+        setTerminusFontSize(Math.max(Number(newFontSize.toFixed(2)), 1));
+    };
+
+    onMount(() => {
+        document.addEventListener('keydown', keydownHandler);
+    });
+
+    onCleanup(() => {
+        document.removeEventListener('keydown', keydownHandler);
+    });
+
     return (
         <div id="text-examples-container">
             <div>
@@ -10,15 +34,26 @@ export function TextExamples() {
             </div>
 
             <div>
-                <div class="tsct-PPMondwest-8">20:56</div>
-                <div class="tsct-PPMondwest-16">20:56</div>
-                <div class="tsct-PPMondwest-16-large">20:56</div>
+                <div
+                    class="tsct-PPMondwest"
+                    style={{
+                        'font-size': `${dynamicFontSize()}px`,
+                    }}
+                >
+                    20:56
+                </div>
+                <div class="tsct-font-size-indicator">Font size: {dynamicFontSize()}px</div>
             </div>
 
             <div>
-                <div class="tsct-PPNeueBit-8">20:56</div>
-                <div class="tsct-PPNeueBit-16">20:56</div>
-                <div class="tsct-PPNeueBit-16-large">20:56</div>
+                <div
+                    class="tsct-PPNeueBit"
+                    style={{
+                        'font-size': `${dynamicFontSize()}px`,
+                    }}
+                >
+                    20:56
+                </div>
             </div>
         </div>
     );
