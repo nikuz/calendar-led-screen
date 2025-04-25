82be23d126fd3d5be93df9d3e79cb12369cb7029
Nikolai <micurino@gmail.com>
2025-04-24 20:36:40 -0700

    ui: add event alarm scaffold

--- src/components/events/EventsAlarm.tsx
+++ src/components/events/EventsAlarm.tsx
@@ -3,19 +3,37 @@ import { useCalendarStateSelect } from 'src/state';
 
 export default function EventsAlarm() {
     let approachingAlarmRef: HTMLAudioElement | undefined;
+    let eventAlarmRef: HTMLAudioElement | undefined;
     const approachingEventIndex = useCalendarStateSelect('approachingEventIndex');
     const activeEventIndex = useCalendarStateSelect('activeEventIndex');
 
     createEffect(() => {
+        if (!approachingAlarmRef || !eventAlarmRef) {
+            return;
+        }
+
         if (approachingEventIndex() !== undefined && activeEventIndex() === undefined) {
-            approachingAlarmRef?.play();
+            approachingAlarmRef.play();
+        } else if (activeEventIndex() !== undefined) {
+            approachingAlarmRef.pause();
+            approachingAlarmRef.currentTime = 0;
+            eventAlarmRef.play();
+        } else if (activeEventIndex() === undefined) {
+            approachingAlarmRef.pause();
+            approachingAlarmRef.currentTime = 0;
+            eventAlarmRef.pause();
+            eventAlarmRef.currentTime = 0;
         }
     });
 
-    return (
+    return <>
         <audio
             ref={approachingAlarmRef}
             src="/event-approaching-alarm.wav"
         />
-    );
+        <audio
+            ref={eventAlarmRef}
+            src="/event-alarm.m4a"
+        />
+    </>;
 }
