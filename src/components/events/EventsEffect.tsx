e17dd62e35a77a3902974cfad94b3d8e9a995a51
Nikolai <micurino@gmail.com>
2025-04-26 14:03:52 -0700

    ui: position event effect relative to event position

--- src/components/events/EventsEffect.tsx
+++ src/components/events/EventsEffect.tsx
@@ -1,12 +1,22 @@
-import { createEffect, onCleanup, on } from 'solid-js';
+import { createSignal, createEffect, onCleanup } from 'solid-js';
 import { useCalendarStateSelect } from 'src/state';
-import { SCREEN_WIDTH, SCREEN_HEIGHT } from 'src/constants';
+import { remapValue } from 'src/utils';
+import {
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    DAY_START_TIME,
+    DAY_END_TIME,
+    EVENTS_ZOOM,
+    EVENT_COLORS,
+} from 'src/constants';
 
 export default function EventsEffect() {
     const activeEvent = useCalendarStateSelect('activeEvent');
     const activeEventIndex = useCalendarStateSelect('activeEventIndex');
     const approachingEventConfirmedIndex = useCalendarStateSelect('approachingEventConfirmedIndex');
-    const waveSpeed = 0.4;
+    const time = useCalendarStateSelect('time');
+    const [effectPosition, setEffectPosition] = createSignal(0);
+    const [effectColor, setEffectColor] = createSignal("#FFF");
     let canvasElRef: HTMLCanvasElement | undefined;
     let animationFrameId: ReturnType<typeof requestAnimationFrame>;
     let offset = 0;
@@ -23,13 +33,12 @@ export default function EventsEffect() {
         ctx.clearRect(0, 0, width, height);
 
         // Constants for wave calculation
-        const centerX = width / 2;
+        const waveSpeed = 0.4;
         const centerY = height - 20;
         const maxRadius = Math.sqrt(width * width + height * height) / 2;
         const waveCount = 20;
         const waveSpacing = maxRadius / waveCount;
         const lineWidth = 2;
-        const waveColor = '#F8E71C';
 
         // Update offset for animation
         offset = (offset + waveSpeed) % waveSpacing;
@@ -46,8 +55,8 @@ export default function EventsEffect() {
 
             // Draw the wave
             ctx.beginPath();
-            ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
-            ctx.strokeStyle = waveColor;
+            ctx.arc(effectPosition(), centerY, radius, 0, Math.PI * 2);
+            ctx.strokeStyle = effectColor();
             ctx.globalAlpha = opacity;
             ctx.lineWidth = lineWidth;
             ctx.stroke();
@@ -63,7 +72,43 @@ export default function EventsEffect() {
     };
 
     const showEffect = () => {
-        animate();
+        cancelAnimationFrame(animationFrameId);
+
+        const event = activeEvent();
+        const eventIndex = activeEventIndex();
+        if (!event || eventIndex === undefined) {
+            return;
+        }
+
+        const startMinutes = event.startDate.getHours() * 60 + event.startDate.getMinutes();
+        const startPosition = Math.round(remapValue({
+            value: startMinutes,
+            inMin: DAY_START_TIME,
+            inMax: DAY_END_TIME,
+            outMin: 0,
+            outMax: SCREEN_WIDTH * EVENTS_ZOOM,
+        }));
+        const endMinutes = event.endDate.getHours() * 60 + event.endDate.getMinutes();
+        const endPosition = Math.round(remapValue({
+            value: endMinutes,
+            inMin: DAY_START_TIME,
+            inMax: DAY_END_TIME,
+            outMin: 0,
+            outMax: SCREEN_WIDTH * EVENTS_ZOOM,
+        }));
+        const eventWidth = endPosition - startPosition;
+        const marginLeft = Math.round(remapValue({
+            value: time().getHours() * 60 + time().getMinutes(),
+            inMin: DAY_START_TIME,
+            inMax: DAY_END_TIME,
+            outMin: 0,
+            outMax: (SCREEN_WIDTH * EVENTS_ZOOM) / (EVENTS_ZOOM / (EVENTS_ZOOM - 1)),
+        }));
+
+        setEffectPosition((startPosition + (eventWidth / 2) - marginLeft));
+        setEffectColor(EVENT_COLORS[eventIndex]);
+
+        animationFrameId = requestAnimationFrame(animate);
     }
 
     const hideEffect = () => {
@@ -74,13 +119,13 @@ export default function EventsEffect() {
         }
     }
 
-    createEffect(on([activeEvent, activeEventIndex, approachingEventConfirmedIndex], () => {
+    createEffect(() => {
         if (activeEvent() && activeEventIndex() !== approachingEventConfirmedIndex()) {
             showEffect();
         } else {
             hideEffect();
         }
-    }));
+    });
 
     onCleanup(() => {
         cancelAnimationFrame(animationFrameId);
