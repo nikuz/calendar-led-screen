7c98f8c74d167a14a736c2d4ac0c781bbe504a49
Nikolai <micurino@gmail.com>
2025-05-03 14:29:45 -0700

    ui: set global game page brightness

--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -1,5 +1,6 @@
 import { onMount, onCleanup, Show } from 'solid-js';
 import { useNavigate } from '@solidjs/router';
+import { useAppStateSelect } from 'src/state';
 import { useGameStateSelect } from './state';
 import {
     SampleCreator,
@@ -15,6 +16,7 @@ import {
 import './GamePage.css';
 
 export default function GamePage() {
+    const brightness = useAppStateSelect('brightness');
     const sampleCreatorIsOpen = useGameStateSelect('sampleCreatorIsOpen');
     const selectedSampleIndex = useGameStateSelect('selectedSampleIndex');
     const gameOver = useGameStateSelect('gameOver');
@@ -39,6 +41,7 @@ export default function GamePage() {
             style={{
                 width: `${SCREEN_WIDTH}px`,
                 height: `${SCREEN_HEIGHT}px`,
+                opacity: brightness() / 100,
             }}
         >
             <Show when={selectedSampleIndex() === undefined && sampleCreatorIsOpen() && !gameOver()}>
